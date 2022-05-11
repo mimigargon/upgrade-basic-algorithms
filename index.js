96d15a46ba9eb3873486ef9f6ca5c1a1afbd1f8b
mimigargon <mireiag77@gmail.com>
2022-05-11 22:57:48 +0200

    Iteration#3

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ let z = h + y;
 //Iteración 2 - Variables avanzadas
 
 //Iteración 3 - Operadores
+
 let numa = 10;
 let numb = 5;
-alert (numa + numb);
+alert (numa * numb);
+
+let nume = 2;
+alert (numa/nume);
+
+let divi = 15;
+let division = 9;
+alert (divi / division);
+
+let y = 10;
+let z = 5;
+let x = y + z;
+alert (x)
+
+let y = 50;
+let z = 5;
+let x = y * z;
+alert (x);
+
