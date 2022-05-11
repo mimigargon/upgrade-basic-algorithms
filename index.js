3ebcc2d3f9ae63c427fd6d9e8eb3631c590fd97f
mimigargon <mireiag77@gmail.com>
2022-05-11 22:45:55 +0200

    Iteration#1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Iteración 1 - Variables
-let myFavoriteHero = Hulk;
+let myFavoriteHero = "Hulk";
 let x = 50;
 let h = 5;
 let y = 10;
@@ -7,4 +7,7 @@ let z = h + y;
 
 //Iteración 2 - Variables avanzadas
 
-
+//Iteración 3 - Operadores
+let numa = 10;
+let numb = 5;
+alert (numa + numb);
