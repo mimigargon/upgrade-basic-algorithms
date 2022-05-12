cf8009e43bcfb411442c826e2bc4c924a382f5ae
mimigargon <mireiag77@gmail.com>
2022-05-12 09:56:11 +0200

    Iteration#3

--- iteration3.js
+++ iteration3.js
@@ -1,19 +1,19 @@
+//1.1
 let numa = 10;
 let numb = 5;
 alert (numa * numb);
-
+//1.2
 let nume = 2;
 alert (numa/nume);
-
+//1.3
 let divi = 15;
 let division = 9;
 alert (divi / division);
-
+//1.4
 let y = 10;
 let z = 5;
 let x = y + z;
 alert (x)
-
-
+//1.5
 x = y * z;
 alert (x);
